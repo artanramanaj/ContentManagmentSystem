579b92c1c9d0550ca52199c55fef74c7c369f0d0
artanOffice <artan.ramanaj@finnie.consulting>
2023-07-06 10:06:58 +0200

    adding reusable files

--- includes/db.php
+++ includes/db.php
@@ -10,29 +10,17 @@ $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if(!$connection) {
     die("database failed to connect");
 } 
-
-$query = "SELECT * FROM category";
-
-$result = mysqli_query($connection, $query);
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> db php </title>
+
 </head>
 <body>
-    <?php
 
-while($row = mysqli_fetch_assoc($result)) {
-    echo $row['cat_id'] . " " . $row['cat_title'];
-    echo "<br>";
-}
-
-    ?>
 </body>
 </html>
